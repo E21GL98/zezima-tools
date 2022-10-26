bdeb8a07d02d52d0d720601f222834016497c7ec
Giulio Laura <116741630+SoNtheZezima@users.noreply.github.com>
2022-10-26 23:17:12 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll("button").forEach((E) => {
-    E.onmousemove = (e) => {
-        let x = e.pageX - e.target.offsetLeft;
-        let y = e.pageY - e.target.offsetTop;
-        e.target.style.setProperty("--x", `${x}px`);
-        e.target.style.setProperty("--y", `${y}px`);
-    };
-});
