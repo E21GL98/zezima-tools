f3a64f873f10022e715255896130909a465419c5
E21GL98 <proudson98@gmail.com>
2022-11-04 17:00:04 +0100

    gradient live

--- effect-site.js
+++ effect-site.js
@@ -1,9 +1,8 @@
 document.querySelectorAll("button").forEach((E) => {
     E.onmousemove = (e) => {
-      let x = e.pageX - e.target.offsetLeft;
-      let y = e.pageY - e.target.offsetTop;
-      e.target.style.setProperty("--x", `${x}px`);
-      e.target.style.setProperty("--y", `${y}px`);
+        let x = e.pageX - e.target.offsetLeft;
+        let y = e.pageY - e.target.offsetTop;
+        e.target.style.setProperty("--x", `${x}px`);
+        e.target.style.setProperty("--y", `${y}px`);
     };
-  });
-  
+});
